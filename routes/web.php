41ec80aaac0ed37ecb248f0643188e12f0927381
jimmy_anaya <jimmyanaya2usp@gmail.com>
2022-01-25 23:22:53 -0500

    creamos las view posts y post generamos la relación post usuario

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [PageController::class, 'posts']);
+Route::get('blog/{post}', [PageController::class, 'post'])->name('post');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
